34b446719be19c5184b3640d0c4a867725afee87
JP Skowron <jpskowron@JP-Skowrons-MacBook-Pro.local>
2015-01-19 10:32:17 -0600

    Initial Commit

---
+++ SuperHeropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperHeropedia
+//
+//  Created by JP Skowron on 1/19/15.
+//  Copyright (c) 2015 JP Skowron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperHeropedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperHeropedia
+//
+//  Created by JP Skowron on 1/19/15.
+//  Copyright (c) 2015 JP Skowron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
